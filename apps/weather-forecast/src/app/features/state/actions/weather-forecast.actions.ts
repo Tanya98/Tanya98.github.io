624b6ce2cdeef76c514851e610cdd67e623be219
tatyana_lysenko <t.lysenko@meshgroup.ru>
2022-05-30 12:26:53 +0200

    do some changes

--- apps/weather-forecast/src/app/features/state/actions/weather-forecast.actions.ts
+++
@@ -1,9 +0,0 @@
-import { createAction } from '@ngrx/store';
-// export const addValue = createAction(
-//     '[Add Value] Add',
-//     props<{ value: string }>()
-// )
-
-export const homeScore = createAction('[Scoreboard Page] Home Score');
-export const awayScore = createAction('[Scoreboard Page] Away Score');
-export const resetScore = createAction('[Scoreboard Page] Score Reset');
